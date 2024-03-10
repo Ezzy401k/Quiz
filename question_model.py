0a6d7d7af3c9eb6cb65b59219f1a6786fde0ce19
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-10 12:51:48 +0300

    added docstring

--- question_model.py
+++ question_model.py
@@ -1,6 +1,11 @@
 class Question:
-
     def __init__(self, question, answer):
-        self.text = question
-        self.answer = answer
+        """
+        Initializes a Question object with the provided question and answer.
 
+        Args:
+        question (str): The text of the question.
+        answer (str): The correct answer to the question.
+        """
+        self.text = question  # Stores the question text
+        self.answer = answer  # Stores the correct answer
