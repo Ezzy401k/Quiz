5cba0642c3357671fc87af29122fda5342dca2c2
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-10 12:50:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,29 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 import os
+
+# Create a list to store Question objects
 question_bank = []
 
+# Populate question_bank with Question objects created from question_data
 for question in question_data:
     question_text = question["text"]
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+# Initialize a QuizBrain object with the question_bank
 quiz = QuizBrain(question_bank)
 
+# Loop through questions until there are no more questions left
 while quiz.still_has_questions():
-
     quiz.next_question()
 
+# Clear the console screen
 os.system('cls')
-print(f"You've completed the quiz, Your final score was : {quiz.score}/{quiz.question_number}")
-input("Tap Enter to Exit!")
+
+# Display the final score to the user
+print(f"You've completed the quiz. Your final score is: {quiz.score}/{quiz.question_number}")
+
+# Wait for user input before exiting
+input("Press Enter to Exit!")
